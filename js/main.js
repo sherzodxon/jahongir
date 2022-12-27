c80447f3de011cb5873386e69716b6856957a906
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2022-12-27 18:03:04 +0500

    project is started

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const form =document.querySelector("#form");
+const Mnumber =form.querySelector("#number");
+const Snumber =form.querySelector("#snumber");
+const Cnumber =form.querySelector("#cnumber");
+const Mname =form.querySelector("#mname");
+const Mtype =form.querySelector("#mtype");
+const Mdate =form.querySelector("#date");
+
+form.addEventListener("submit",function(evt){
+    evt.preventDefault();
+    const elements =e
+})
